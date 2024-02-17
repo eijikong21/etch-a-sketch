44364a8443d6b888140ee4355060b9c432f4a6f4
eijikong21 <eijinakayama21@gmail.com>
2024-02-17 22:50:26 +0800

    Added working button and prompt, just needs to align divs properly based on numbers provided.

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
-const container = document.querySelector('#container');
+
 const btn = document.querySelector("#btn");
-let gridSize;
-function addDiv(){
-    btn.addEventListener('click',function (){
-        gridSize = parseInt(prompt("Enter Grid Size",0));
-        console.log(typeof gridSize);
-        console.log(gridSize);
-      
-        if(gridSize==NaN || gridSize<0 || gridSize>100){
-            console.log("Error");
-            return "";
-        }
-        test();
-    });
-for(j=0;j<256;j++){
-const newDiv = document.createElement('div');
+
+
+
+function removeDivs(){
+    let container = document.querySelector('#container');
+    if (container) {
+        container.remove();
+    }
+}
+function addDiv(gridSize){
+        newDiv = document.createElement('div');
+        newDiv.id="container";
+        document.body.appendChild(newDiv);
+         container = document.querySelector('#container');
+    let i=gridSize; 
+    i=i**2;
+for(j=0;j<i;j++){
+
+let newDiv = document.createElement('div');
 newDiv.className = "newdiv";
 container.appendChild(newDiv);
-
 newDiv.addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = "purple";
+event.target.style.backgroundColor = "purple";
 });
 }
 
 }
+btn.addEventListener('click',function (){
+    gridSize = parseInt(prompt("Enter Grid Size",0));
+    console.log(typeof gridSize);
+    console.log(gridSize);
+    if(gridSize==NaN || gridSize<0 || gridSize>100){
+        console.log("Error");
+        return "";
+    }
+    removeDivs();
+    addDiv(gridSize);
+});
 
-
-addDiv();
+addDiv(16);
 
 function test(){
     console.log(gridSize);
