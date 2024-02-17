798505983921904bf2e0dad6139212f4333af857
eijikong21 <eijinakayama21@gmail.com>
2024-02-17 21:06:21 +0800

    Added button with prompt converted to number already with restrictions to nan, numbers above 100 and less than 0, color hover on div also added.

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const container = document.querySelector('#container');
+const btn = document.querySelector("#btn");
+let gridSize;
 function addDiv(){
-    for(i=0;i<16;i++){
+    btn.addEventListener('click',function (){
+        gridSize = parseInt(prompt("Enter Grid Size",0));
+        console.log(typeof gridSize);
+        console.log(gridSize);
+      
+        if(gridSize==NaN || gridSize<0 || gridSize>100){
+            console.log("Error");
+            return "";
+        }
+        test();
+    });
+for(j=0;j<256;j++){
 const newDiv = document.createElement('div');
 newDiv.className = "newdiv";
 container.appendChild(newDiv);
-    }
+
+newDiv.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "purple";
+});
 }
 
-for(j=0;j<16;j++){
+}
+
+
 addDiv();
+
+function test(){
+    console.log(gridSize);
 }
