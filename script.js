451553ab5974272d3e0d16277ea7fe32a152aac0
eijikong21 <eijinakayama21@gmail.com>
2024-02-18 21:01:37 +0800

    Made boxes made every row and once that row is at expected grid size create new column and repeats until column count is equal to row.

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
 
 const btn = document.querySelector("#btn");
 
-
-
 function removeDivs(){
     let container = document.querySelector('#container');
     if (container) {
         container.remove();
     }
 }
+function addDivContainer(){
+    let newDivRow = document.createElement('div');
+    container.appendChild(newDivRow);
+    newDivRow.className = "divContainer";
+    const divContainer = document.querySelector('.divContainer');
+}
 function addDiv(gridSize){
+    let i=gridSize; 
+    let y=gridSize;
         newDiv = document.createElement('div');
         newDiv.id="container";
         document.body.appendChild(newDiv);
-         container = document.querySelector('#container');
-    let i=gridSize; 
-    i=i**2;
-for(j=0;j<i;j++){
-
+        container = document.querySelector('#container');
+        for(j=1;j<=i;j++){
+        let newDivRow = document.createElement('div');
+        container.appendChild(newDivRow);
+        newDivRow.className = "divContainer";
+        var divContainer = document.querySelectorAll('.divContainer');
+        }
+      z=0;
+for(j=1;j<=y;j++){
 let newDiv = document.createElement('div');
-newDiv.className = "newdiv";
-container.appendChild(newDiv);
+divContainer[z].appendChild(newDiv);
+newDiv.className ="newdiv";
 newDiv.addEventListener("mouseover", (event) => {
 event.target.style.backgroundColor = "purple";
 });
+if(j==gridSize){
+    z++;
+    j=0;
+    if(z==gridSize){
+        break;
+    }
+   }
 }
 
 }
@@ -39,8 +56,4 @@ btn.addEventListener('click',function (){
     addDiv(gridSize);
 });
 
-addDiv(16);
-
-function test(){
-    console.log(gridSize);
-}
+addDiv(4);
